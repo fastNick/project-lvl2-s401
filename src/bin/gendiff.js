5b7d2cbc3ce27ae0f065729fe9a66c177f5b9fc5
fastNick88 <fastNick88@gmail.com>
2019-07-10 20:02:14 +0300

    Create difference calculator project. Step1

---
+++ src/bin/gendiff.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import commander from '../options_parser/commander'
+
+commander();
---
+++ src/lib/helper.js
@@ -0,0 +1,3 @@
+export default (firstConfig, secondConfig, config) => {  
+  console.log(`Calculate difference between firstConfig and secondCongig`)  
+}
