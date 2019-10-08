36375876db2f2240f3b986a35b9780678feda52c
Nick <fastNick88@gmail.com>
2019-10-08 19:28:28 +0300

    Move io logic from low-level abstract functions to upper level

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import getGenDiff from './gendiff';
+import { readFileSync } from 'fs';
+import { extname } from 'path';
+import convertToObject from './parsers';
+import getAst from './ast';
 import getRender from './renders';
 
 const renderGenDiff = (beforePath, afterPath, format) => {
-  const genDiff = getGenDiff(beforePath, afterPath);
-  return getRender(genDiff, format);
+  const beforeContent = readFileSync(beforePath, 'utf8');
+  const beforeDataType = extname(beforePath);
+  const beforeConfig = convertToObject(beforeDataType, beforeContent);
+  const afterContent = readFileSync(afterPath, 'utf8');
+  const afterDataType = extname(afterPath);
+  const afterConfig = convertToObject(afterDataType, afterContent);
+  const gendiff = getAst(beforeConfig, afterConfig);
+  return getRender(gendiff, format);
 };
 
 export default renderGenDiff;
--- src/io.js
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'fs';
-import { extname } from 'path';
-
-export const getExtension = source => extname(source);
-
-export const getContent = source => readFileSync(source, 'utf8');
