a127d804989343232684d60f6a892af044fde859
fastNick88 <fastNick88@gmail.com>
2019-12-21 14:12:40 +0200

    Rename test files and update getting path functionality. Step: mentor comments

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -9,17 +9,17 @@ import {
 
 const getContent = path => readFileSync(path, 'utf8');
 
+const getPath = file => resolve(__dirname, fixturesPath, `${file}`);
+
 describe(`Calculate difference for recursive structures of [${dataTypes}] types of data for different [${formats}] formats `, () => {
   formats.forEach((format) => {
     test.each(dataTypes)(
       `Test functionality of gendiff for .%s files in form of ${format} format`,
       (dataType) => {
-        const beforePath = resolve(__dirname,
-          fixturesPath, `./before.${dataType}`);
-        const afterPath = resolve(__dirname,
-          fixturesPath, `./after.${dataType}`);
-        const expectedContent = getContent(resolve(__dirname,
-          fixturesPath, `./${format}.txt`));
+        const beforePath = getPath(`./before.${dataType}`);
+        const afterPath = getPath(`./after.${dataType}`);
+        const resultPath = getPath(`./expected_${format}.txt`);
+        const expectedContent = getContent(resultPath);
         const genDiffResult = gendiff(beforePath, afterPath, format);
         expect(genDiffResult).toEqual(expectedContent);
       },
