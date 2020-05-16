0723ac4e0c5092866ddd2ff8ff6e5091cbe9da4e
fastNick88 <fastNick88@gmail.com>
2020-05-16 19:21:39 +0300

    Update logic of nested formatter

--- src/formatters/nested.js
+++ src/formatters/nested.js
@@ -1,6 +1,5 @@
 import { flattenDeep, isPlainObject } from 'lodash';
 
-const initialPaddingLength = 0;
 const paddingBeforeSign = 2;
 
 const signsByNode = {
@@ -9,41 +8,40 @@ const signsByNode = {
   'not changed': ' ',
   nested: ' ',
 };
-const getBeforeKeyString = nodeType => `${' '.repeat(paddingBeforeSign)}${signsByNode[nodeType]} `;
+const getBeforeKeyString = (nodeType) => `${' '.repeat(paddingBeforeSign)}${signsByNode[nodeType]} `;
 
 const getObjectProperty = (value, key, parentPadding) => `${' '
   .repeat(parentPadding)}${getBeforeKeyString('not changed')}${key}: ${value[key]}\n`;
 
-const stringifyObject = (nodeValue, parentPadding) => {
-  const properties = Object.keys(nodeValue)
-    .map(key => getObjectProperty(nodeValue, key, parentPadding));
-  const flattenProperties = properties.join('');
+const stringifyObject = (node, initialPadding) => {
+  const parentPadding = getBeforeKeyString(node.type).length + initialPadding;
+  const flattenProperties = Object.keys(node.value)
+    .map((key) => getObjectProperty(node.value, key, parentPadding)).join('');
   return `{\n${flattenProperties}${' '.repeat(parentPadding)}}`;
 };
 
-const generateBaseString = (key, initialPadding, type, nodeValue) => {
-  const totalKeyPadding = getBeforeKeyString(type).length + initialPadding;
-  const value = isPlainObject(nodeValue) ? stringifyObject(nodeValue, totalKeyPadding)
-    : nodeValue;
-  const beforeKey = ' '.repeat(initialPadding) + getBeforeKeyString(type);
-  return `${beforeKey}${key}: ${value}\n`;
+const generateBaseString = (node, initialPadding) => {
+  const value = isPlainObject(node.value) ? stringifyObject(node, initialPadding)
+    : node.value;
+  const beforeKey = ' '.repeat(initialPadding) + getBeforeKeyString(node.type);
+  return `${beforeKey}${node.key}: ${value}\n`;
 };
 
-const rec = ([head, ...rest], initialPadding) => (head
-  ? [stringifiersByType[head.type](head, initialPadding), ...rec(rest, initialPadding)] : []);
+const rec = ([head, ...rest], stringifiersByTypeObject, initialPadding = 0) => (head
+  ? [stringifiersByTypeObject[head.type](head, initialPadding),
+    ...rec(rest, stringifiersByTypeObject, initialPadding)] : []);
 
 const stringifiersByType = ({
-  deleted: (renderNode, initialPadding,
-    value = renderNode.value) => generateBaseString(renderNode.key,
-    initialPadding, renderNode.type, value),
-  inserted: (renderNode, initialPadding,
-    value = renderNode.value) => generateBaseString(renderNode.key,
-    initialPadding, renderNode.type, value),
-  changed: (renderNode, initialPadding) => [generateBaseString(renderNode.key, initialPadding, 'deleted', renderNode.valueOld),
-    generateBaseString(renderNode.key, initialPadding, 'inserted', renderNode.valueNew)],
+  deleted: (renderNode, initialPadding) => generateBaseString(renderNode, initialPadding),
+  inserted: (renderNode, initialPadding) => generateBaseString(renderNode, initialPadding),
+  'not changed': (renderNode, initialPadding) => generateBaseString(renderNode, initialPadding),
+  changed: (renderNode, initialPadding) => [
+    generateBaseString({ key: renderNode.key, type: 'deleted', value: renderNode.valueOld }, initialPadding),
+    generateBaseString({ key: renderNode.key, type: 'inserted', value: renderNode.valueNew }, initialPadding),
+  ],
   nested: (renderNode, initialPadding) => {
     const totalPadding = getBeforeKeyString(renderNode.type).length + initialPadding;
-    const children = rec(renderNode.children, totalPadding);
+    const children = rec(renderNode.children, stringifiersByType, totalPadding);
     return [
       ' '.repeat(initialPadding),
       getBeforeKeyString(renderNode.type),
@@ -55,11 +53,9 @@ const stringifiersByType = ({
       '}\n',
     ];
   },
-  'not changed': (renderNode, initialPadding) => generateBaseString(renderNode.key, initialPadding, renderNode.type, renderNode.value),
 });
 
 export default (ast) => {
-  const stringifiers = ast.map(renderNode => stringifiersByType[renderNode
-    .type](renderNode, initialPaddingLength));
+  const stringifiers = rec(ast, stringifiersByType);
   return flattenDeep(['{\n', stringifiers, '}']).join('');
 };
